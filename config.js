8f93fd0b4704d2b107bb6aa87438d986688653e6
ChuongC <154713231+ChuongC@users.noreply.github.com>
2024-04-30 00:24:54 +0700

    Add files via upload
    Create HPBD for 2024

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Baby Cute';
+const giftUrl = '';
+const eventName = 'Happy Birthday';
+const titleCard = 'Dearest My Bà Nhà';
+const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
