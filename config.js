aa93924dc6d59e58de941ee08fff5bcf49e7223a
CaoVanChuong <chuongb2111788@student.ctu.edu.vn>
2024-04-30 01:01:44 +0700

    content

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ const nameGirl = 'Baby Cute';
 const giftUrl = '';
 const eventName = 'Happy Birthday';
 const titleCard = 'Dearest My Bà Nhà';
-const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+const contentCard = 'Happy birthday, my baby cutie! You know, there is no word that can express my gratitude to you because you come into my life and make it cooler and cooler, and that is always the greatest thing for me. Our lovely and beautiful memories together will never stop, and a big life is waiting for us ahead. Hand in hand, we are going to go through it all. Not only for several days, months, or even years, but actually it is a life. I wish my world is you with my best regards: always have good luck, lots of joy and happiness, and always smile brightly. LOVE and MISS YOU BABY!';
 
 
 const giftImage = 'hot-girl.jpg';
